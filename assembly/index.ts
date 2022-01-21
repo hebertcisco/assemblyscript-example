78ca5f57d3db179917c05a5141bf0330850b4589
Hebert  F. Barros <hebertcisco@outlook.com>
2022-01-21 10:10:52 -0300

    feat: :sparkles: Calculates the n-th Fibonacci number
    > :sparkles: Calculates the n-th Fibonacci number with *assemblyscript*

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,5 +1,13 @@
-// The entry file of your WebAssembly module.
-
-export function add(a: i8, b: i16): i32 {
-  return a + b;
+/** Calculates the n-th Fibonacci number. */
+export function fib(n: i32): i32 {
+  var a = 0, b = 1
+  if (n > 0) {
+    while (--n) {
+      let t = a + b
+      a = b
+      b = t
+    }
+    return b
+  }
+  return a
 }
